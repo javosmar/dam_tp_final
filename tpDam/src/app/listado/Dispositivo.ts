7d64927f006b9e8256b81bef8b7c969fc6b0c4df
Martin Acosta <javosmar@gmail.com>
2020-07-09 17:37:49 -0300

    hasta punto 5
    Se concluyó la primera parte del tp, se podría desacoplar los dispositivos con un nuevo componente con lo visto en la clase 3 (@input y @output)

---
+++ tpDam/src/app/listado/Dispositivo.ts
@@ -0,0 +1,40 @@
+export class Dispositivo{
+    private _dispositivoId;
+    private _nombre;
+    private _ubicacion;
+    private _electrovalvulaId;
+    
+    constructor(dispId:number,nombre:string,ubicacion:string,electovalId:number){
+        this._dispositivoId = dispId;
+        this._nombre = nombre;
+        this._ubicacion = ubicacion;
+        this._electrovalvulaId = electovalId;
+    }
+    public get dispositivoId() {
+        return this._dispositivoId;
+    }
+    public set dispositivoId(value) {
+        this._dispositivoId = value;
+    }
+    public get nombre() {
+        return this._nombre;
+    }
+    public set nombre(value) {
+        this._nombre = value;
+    }
+    public get ubicacion() {
+        return this._ubicacion;
+    }
+    public set ubicacion(value) {
+        this._ubicacion = value;
+    }
+    public get electrovalvulaId() {
+        return this._electrovalvulaId;
+    }
+    public set electrovalvulaId(value) {
+        this._electrovalvulaId = value;
+    }
+
+
+
+}
